136e87fae3dafbab34d0496225f78ac175f96027
Ryrann <90543129+iamryran@users.noreply.github.com>
2024-05-27 14:37:26 +0700

    reset

--- app/location-enterprise/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-
-const ResultPageDetail = dynamic(() => import('@/components/Location'), { ssr: false });
-
-const ResultPage = () => <ResultPageDetail />;
-
-export default ResultPage;
